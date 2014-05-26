a99f484abb1d95a974f995742761c3b6ce9820a4
Qix <i.am.qix@gmail.com>
2014-05-26 13:05:32 -0600

    added gulp shell as a dev dependency

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,3 +8,5 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var peg = require('gulp-peg');
 var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var shell = require('gulp-shell');
