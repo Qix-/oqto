330aec08dfb75e435ed7cb440d867272b71f5363
Qix <i.am.qix@gmail.com>
2014-05-26 15:29:05 -0600

    added source tree building

--- Gulpfile.js
+++ Gulpfile.js
@@ -56,6 +56,43 @@ gulp.task('dep-ace', function() {
 
 gulp.task('deps', ['dep-ace', 'dep-pegace'], function () {});
 
+gulp.task('src-dev-coffee', function() {
+	gulp.src('./src/dev/**/*.coffee')
+		.pipe(coffee())
+		.pipe(rename(function(path) {
+				path.dirname = path.dirname.replace('src/dev/', '')
+			}))
+		.pipe(gulp.dest('./dev/'));
+});
+
+gulp.task('src-dev', ['src-dev-coffee'], function() {
+	gulp.src('./src/dev/**/*')
+		.pipe(rename(function(path) {
+				path.dirname = path.dirname.replace('src/dev/', '')
+			}))
+		.pipe(gulp.dest('./dev/'));
+});
+
+gulp.task('src-lib', function() {
+	gulp.src('./src/lib/**/*.coffee')
+		.pipe(coffee())
+		.pipe(rename(function(path) {
+				path.dirname = path.dirname.replace('src/lib/', '')
+			}))
+		.pipe(gulp.dest('./lib/'));
+});
+
+gulp.task('src-bin', function() {
+	gulp.src('./src/oqto.coffee')
+		.pipe(coffee())
+		.pipe(rename(function(path) {
+				path.dirname = path.dirname.replace('src/', '')
+			}))
+		.pipe(gulp.dest('./bin/'));
+});
+
+gulp.task('src', ['src-dev', 'src-bin', 'src-lib'], function() {});
+
 gulp.task('default', ['clean'], function() {
-	gulp.start('deps');
+	gulp.start('deps', 'src');
 });
