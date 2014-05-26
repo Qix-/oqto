e780101709f6634571fab0072d9eb26b849de480
Qix <i.am.qix@gmail.com>
2014-05-26 12:57:20 -0600

    added gulp/build dev dependencies

---
+++ Gulpfile.js
@@ -0,0 +1,10 @@
+/**
+ * Oqto
+ *
+ *	Gulpfile / build script for Oqto
+ */
+
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var peg = require('gulp-peg');
+var rename = require('gulp-rename');
